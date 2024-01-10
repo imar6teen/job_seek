05519cf27bcbfcf1f00065c7066e046cb9e5d49e
Aimar Anand <aimaranand02@gmail.com>
2024-01-10 14:33:25 +0800

    bootstrap app

---
+++ src/config/database.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: "root",
+    password: null,
+    database: "job_seek_dev",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "job_seek_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "job_seek_prod",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
